345520552fce153dc23269b04117682219e881e8
Ivan Rodriguez <culma6794@gmail.com>
2022-04-22 18:34:26 -0500

    salvando projecto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,9 +39,6 @@
 
     @yield('content')
 
-    <footer>
-        todos los derechos reservados
-    </footer>
     
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="{{asset('js/dinamic.js')}}"></script>
